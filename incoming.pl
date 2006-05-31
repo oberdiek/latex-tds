ea6529084ba9929b5dbd2b71804c54b281f2cf4e
Heiko Oberdiek <heiko.oberdiek@googlemail.com>
2006-05-31 09:22:12 +0000

    * AMS download added.

--- incoming.pl
+++ incoming.pl
@@ -3,8 +3,11 @@ use strict;
 $^W=1;
 
 my $dir_incoming = 'incoming';
+my $dir_incoming_ams = "$dir_incoming/ams";
 my $url_ctan = 'ftp://dante.ctan.org/tex-archive/macros/latex';
+my $url_ams = 'ftp://ftp.ams.org/pub/tex';
 my $prg_wget = 'wget -N';
+my $prg_curl = 'curl';
 chomp(my $cwd = `pwd`);
 
 my @required_list = (
@@ -17,6 +20,8 @@ my @required_list = (
     'psnfss'
 );
 
+# CTAN download
+
 -d $dir_incoming or mkdir $dir_incoming;
 chdir $dir_incoming;
 
@@ -26,5 +31,25 @@ foreach my $pkg (@required_list) {
 }
 
 chdir $cwd;
+
+# AMS download
+
+download_ams('amslatex.zip');
+download_ams('amsrefs.zip');
+
+sub download_ams {
+    my $file = $_[0];
+    my $url = "$url_ams/$file";
+    my $file = "$dir_incoming_ams/$file";
+    print "* $url\n";
+    -d $dir_incoming_ams or mkdir $dir_incoming_ams;
+    my $cmd = $prg_curl;
+    $cmd .= " --disable-epsv";                #
+    $cmd .= " --time-cond $file" if -f $file; # download only if newer
+    $cmd .= " --remote-time";                 # set file date
+    $cmd .= " --output $file";                # target file
+    $cmd .= " $url";                          # url
+    system($cmd);
+}
 
 __END__
