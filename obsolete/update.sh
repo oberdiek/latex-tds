03fc140531adaefc9ae553e22839eaac7aa27290
Heiko Oberdiek <heiko.oberdiek@googlemail.com>
2006-06-04 21:31:45 +0000

    * readme.txt added.

--- obsolete/update.sh
+++ obsolete/update.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 scp distrib/*.zip dbis:.public_html/tmp/latex-tds/
 ssh dbis "chmod 644 .public_html/tmp/latex-tds/*-tds.zip"
-scp index.html dbis:.public_html/tmp/latex-tds/
+scp index.html readme.txt dbis:.public_html/tmp/latex-tds/
 ssh dbis "chmod 644 .public_html/tmp/latex-tds/index.html"
+ssh dbis "chmod 644 .public_html/tmp/latex-tds/readme.txt"
