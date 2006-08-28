5510a3385cbf513a8c4c694d97c2ecd6702da1ed
Heiko Oberdiek <heiko.oberdiek@googlemail.com>
2006-08-28 09:42:43 +0000

    * Typo fixed.

--- build.pl
+++ build.pl
@@ -487,7 +487,7 @@ section('Install tex doc');
             sample2e.tex
             small2e.tex
         ];
-        install 'texm/tex/latex/base', qw[
+        install 'texmf/tex/latex/base', qw[
             texsys.cfg
         ];
         chdir $cwd;
