2cd55e37786a62c245bec6e30cc6a63ec5213ae3
Heiko Oberdiek <heiko.oberdiek@googlemail.com>
2013-01-24 21:06:53 +0000

    * Licenses in source module updated and reorganized.

--- build.pl
+++ build.pl
@@ -1710,7 +1710,8 @@ if ($modules{'source'}) {
     install "$dir_dest/patch", glob("$dir_patch/*.*");
     install "$dir_dest/lib", $file_ziptimetree;
     install "$dir_dest/lib", $file_adjust_checksum;
-    install "$dir_dest/license", "$dir_license/lppl.txt";
+    install "$dir_dest/license/latex-tds", "$dir_license/latex-tds/lppl.txt";
+    install "$dir_dest/license/adjust_checksum", "$dir_license/adjust_checksum/lppl.txt";
     install "$dir_dest/license/ziptimetree", "$dir_license/ziptimetree/lgpl.txt";
     install $dir_distrib, 'README';
 }
